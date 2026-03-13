6ab48588e1e9c63bcbcbdc1f97701a05f1f2a6e5
dpylak <203235923+dawpyl@users.noreply.github.com>
2026-03-13 14:07:05 +0100

    feat: remove redundant mapper configuration

--- src/main/java/org/onecx/document/management/rs/v1/mappers/DocumentMapper.java
+++ src/main/java/org/onecx/document/management/rs/v1/mappers/DocumentMapper.java
@@ -25,7 +25,7 @@ import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
 
 import gen.org.onecx.document.management.rs.v1.model.*;
 
-@Mapper(componentModel = "jakarta-cdi", uses = OffsetDateTimeMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(uses = OffsetDateTimeMapper.class)
 public interface DocumentMapper {
     @Mapping(target = "removeTagsItem", ignore = true)
     @Mapping(target = "removeDocumentRelationshipsItem", ignore = true)
